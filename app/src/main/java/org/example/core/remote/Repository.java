a081a2e561b66265a2afe03594f51a653cc55860
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-03 16:42:42 +0100

    feat: add new methods, init remote repo impl

--- app/src/main/java/org/example/core/remote/Repository.java
+++ app/src/main/java/org/example/core/remote/Repository.java
@@ -3,5 +3,7 @@ package org.example.core.remote;
 import java.util.List;
 
 public interface Repository {
+    void addDirectory(RemoteDirectory directory);
+    void addFile(RemoteFile file);
     List<RemoteDirectory> getRemoteDirectories();
 }
--- app/src/main/java/org/example/core/remote/RepositoryImpl.java
+++ app/src/main/java/org/example/core/remote/RepositoryImpl.java
@@ -1,2 +1,20 @@
-package org.example.core.remote;public class RepositoryImpl {
+package org.example.core.remote;
+
+import java.util.List;
+
+public class RepositoryImpl implements Repository{
+    @Override
+    public void addDirectory(RemoteDirectory directory) {
+
+    }
+
+    @Override
+    public void addFile(RemoteFile file) {
+
+    }
+
+    @Override
+    public List<RemoteDirectory> getRemoteDirectories() {
+        return null;
+    }
 }
