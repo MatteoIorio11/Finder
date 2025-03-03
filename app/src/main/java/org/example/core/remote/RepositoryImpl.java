8a14ac5dda2d1f38f2436902c971e003d4ec11cf
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-03 17:21:14 +0100

    feat: add partial implementation

--- app/src/main/java/org/example/core/remote/RepositoryImpl.java
+++ app/src/main/java/org/example/core/remote/RepositoryImpl.java
@@ -1,20 +1,34 @@
 package org.example.core.remote;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class RepositoryImpl implements Repository{
+
+    private final List<RemoteDirectory> remoteDirectories;
+    private final List<RemoteFile> remoteFiles;
+
+    public RepositoryImpl() {
+        this.remoteDirectories = new LinkedList<>();
+        this.remoteFiles = new LinkedList<>();
+    }
     @Override
     public void addDirectory(RemoteDirectory directory) {
-
+        this.remoteDirectories.add(directory);
     }
 
     @Override
     public void addFile(RemoteFile file) {
-
+        this.remoteFiles.add(file);
     }
 
     @Override
     public List<RemoteDirectory> getRemoteDirectories() {
-        return null;
+        return this.remoteDirectories;
+    }
+
+    @Override
+    public List<RemoteFile> getRemoteFiles() {
+        return this.remoteFiles;
     }
 }
