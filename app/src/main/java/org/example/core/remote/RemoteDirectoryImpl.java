1574f95c1fbc171e81dde4822a3dd2d522b93040
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-03 17:20:54 +0100

    feat: add partial implementation of remote directory

--- app/src/main/java/org/example/core/remote/RemoteDirectoryImpl.java
+++ app/src/main/java/org/example/core/remote/RemoteDirectoryImpl.java
@@ -1,11 +1,27 @@
 package org.example.core.remote;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class RemoteDirectoryImpl implements RemoteDirectory{
+    private final String name, remoteUrl;
+    private final List<RemoteFile> remoteFiles;
+    private final List<RemoteDirectory> remoteDirectories;
+    public RemoteDirectoryImpl(final String name, final String remoteUrl) {
+        this.name = Objects.requireNonNull(name);
+        this.remoteUrl = Objects.requireNonNull(remoteUrl);
+        this.remoteFiles = new LinkedList<>();
+        this.remoteDirectories = new LinkedList<>();
+    }
     @Override
     public String getName() {
-        return null;
+        return this.name;
+    }
+
+    @Override
+    public String getRemoteUrl() {
+        return this.remoteUrl;
     }
 
     @Override
@@ -14,12 +30,19 @@ public class RemoteDirectoryImpl implements RemoteDirectory{
     }
 
     @Override
-    public void addFile(RemoteFile file) {
-
+    public void addFile(final RemoteFile file) {
+        this.remoteFiles.add(file);
     }
 
     @Override
-    public void addDirectory(RemoteDirectory directory) {
+    public void addDirectory(final RemoteDirectory directory) {
+        this.remoteDirectories.add(directory);
+    }
 
+    @Override
+    public String toString() {
+        return "RemoteDirectoryImpl{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
