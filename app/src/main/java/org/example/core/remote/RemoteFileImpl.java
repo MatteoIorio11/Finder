a44e51c78d9c79ec165f01c31ebab38f8eda95ab
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-03 17:20:42 +0100

    feat: add partial implementation of remote file

--- app/src/main/java/org/example/core/remote/RemoteFileImpl.java
+++ app/src/main/java/org/example/core/remote/RemoteFileImpl.java
@@ -1,2 +1,30 @@
-package org.example.core.remote;public class RemoteFileImpl {
+package org.example.core.remote;
+
+public class RemoteFileImpl implements RemoteFile{
+
+    private final String name;
+    public RemoteFileImpl(final String name) {
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getRemoteUrl() {
+        return null;
+    }
+
+    @Override
+    public String getContent() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteFileImpl{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
