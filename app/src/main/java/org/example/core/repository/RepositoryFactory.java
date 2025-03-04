4cb2278c063e9559bfd1d75fd1841d65fc3d2f5e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 17:09:01 +0100

    refactor: use new method for create the remote repository

--- app/src/main/java/org/example/core/repository/RepositoryFactory.java
+++ app/src/main/java/org/example/core/repository/RepositoryFactory.java
@@ -2,15 +2,16 @@ package org.example.core.repository;
 
 import org.example.core.repository.local.LocalRepository;
 import org.example.core.repository.remote.RemoteRepository;
-import org.example.core.scraper.remote.HttpAgent;
+import org.example.core.scraper.remote.RemoteScraperImpl;
 
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.Objects;
+import java.util.Optional;
 
 public class RepositoryFactory {
     public static RemoteRepository remoteRepository(final URL repositoryUrl, final String token) {
-        return HttpAgent.getRemoteRepository(Objects.requireNonNull(repositoryUrl), Objects.requireNonNull(token));
+        return new RemoteScraperImpl().getRepository(Objects.requireNonNull(repositoryUrl), Optional.of(Objects.requireNonNull(token)));
     }
 
     public static LocalRepository localRepository(final Path repositoryPath) {
