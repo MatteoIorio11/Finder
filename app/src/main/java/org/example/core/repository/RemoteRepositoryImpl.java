3d4590ccf7d1d19a7f461519a2cbbaca3c58b137
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 14:25:31 +0100

    refactor: rename into RemoteRepository

--- app/src/main/java/org/example/core/repository/RemoteRepositoryImpl.java
+++ app/src/main/java/org/example/core/repository/RemoteRepositoryImpl.java
@@ -2,27 +2,26 @@ package org.example.core.repository;
 
 import org.example.core.remote.RemoteDirectory;
 import org.example.core.remote.RemoteFile;
-import org.example.core.repository.Repository;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class RepositoryImpl implements Repository<RemoteDirectory, RemoteFile> {
+public class RemoteRepositoryImpl implements Repository<RemoteDirectory, RemoteFile> {
 
     private final List<RemoteDirectory> remoteDirectories;
     private final List<RemoteFile> remoteFiles;
 
-    public RepositoryImpl() {
+    public RemoteRepositoryImpl() {
         this.remoteDirectories = new LinkedList<>();
         this.remoteFiles = new LinkedList<>();
     }
     @Override
-    public void addDirectory(RemoteDirectory directory) {
+    public void addDirectory(final RemoteDirectory directory) {
         this.remoteDirectories.add(directory);
     }
 
     @Override
-    public void addFile(RemoteFile file) {
+    public void addFile(final RemoteFile file) {
         this.remoteFiles.add(file);
     }
 
