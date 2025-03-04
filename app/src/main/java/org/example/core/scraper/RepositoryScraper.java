46b883cb60067e65068e2a5512abd25a9b58afc2
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 15:16:38 +0100

    feat: use strategy inside repository scraper

--- app/src/main/java/org/example/core/scraper/RepositoryScraper.java
+++
@@ -1,9 +0,0 @@
-package org.example.core.scraper;
-
-import org.example.core.remote.RemoteCollection;
-
-import java.util.Optional;
-
-public interface RepositoryScraper {
-    Optional<RemoteCollection> scrape(String htmlPage);
-}
--- app/src/main/java/org/example/core/scraper/RepositoryScraperImpl.java
+++ app/src/main/java/org/example/core/scraper/RepositoryScraperImpl.java
@@ -4,31 +4,39 @@ import org.example.core.remote.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 import org.jsoup.safety.Safelist;
+import org.w3c.tidy.Tidy;
 
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
-public class RepositoryScraperImpl implements RepositoryScraper {
+public class RepositoryScraperImpl {
     private final static String GITHUB = "https://github.com";
-    @Override
-    public Optional<RemoteCollection> scrape(final String htmlPage) {
-        final Document document = Jsoup.parse(htmlPage);
-        final List<RemoteFile> files = new LinkedList<>();
-        final List<RemoteDirectory> directories = new LinkedList<>();
-        document.stream().filter(element -> element.id().contains("folder-row-"))
-                .map(element -> element.selectFirst("a"))
-                .filter(element -> !(element.text().equals("..") || element.text().equals(".")))
-                .filter(element -> element.hasAttr("aria-label"))
-                .forEach(aTag-> {
-                    final String newPath = GITHUB + aTag.attribute("href").getValue();
-                    if (aTag.attr("aria-label").contains("File")) {
-                        files.add(new RemoteFileImpl(aTag.text(), newPath));
-                    } else {
-                        directories.add(new RemoteDirectoryImpl(aTag.text(), newPath));
-                    }
-                });
-        return Optional.of(new RemoteCollection(files, directories));
+
+    static public Optional<RemoteCollection> scrape(final String htmlPage) {
+        try {
+            final Document document = Jsoup.parse(htmlPage, Parser.htmlParser());
+            final List<RemoteFile> files = new LinkedList<>();
+            final List<RemoteDirectory<RemoteFile>> directories = new LinkedList<>();
+            document.stream().filter(element -> element.id().contains("folder-row-"))
+                    .map(element -> element.selectFirst("a"))
+                    .filter(element -> !(element.text().equals("..") || element.text().equals(".")))
+                    .filter(element -> element.hasAttr("aria-label"))
+                    .forEach(aTag -> {
+                        final String newPath = GITHUB + aTag.attribute("href").getValue();
+                        if (aTag.attr("aria-label").contains("File")) {
+                            files.add(new RemoteFileImpl(aTag.text(), newPath));
+                        } else {
+                            directories.add(new RemoteDirectoryImpl(aTag.text(), newPath));
+                        }
+                    });
+            return Optional.of(new RemoteCollection(files, directories));
+        }catch (Exception ignored) {
+            return Optional.empty();
+        }
     }
 }
