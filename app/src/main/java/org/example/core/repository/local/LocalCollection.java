bee4100638a1ebeef213ae2e3ea8aef8af52fcd8
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 17:42:08 +0100

    feat: create records and implement RepositoryCollection

--- app/src/main/java/org/example/core/repository/local/LocalCollection.java
+++ app/src/main/java/org/example/core/repository/local/LocalCollection.java
@@ -1,2 +1,28 @@
-package org.example.core.repository.local;public record LocalCollection() {
+package org.example.core.repository.local;
+
+import org.example.core.repository.RepositoryCollection;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public record LocalCollection(List<LocalFile> files, List<LocalDirectory> directories) implements RepositoryCollection<Path, LocalDirectory, LocalFile> {
+    @Override
+    public void setFiles(List<LocalFile> files) {
+        this.files.addAll(files);
+    }
+
+    @Override
+    public void setDirectories(List<LocalDirectory> directories) {
+        this.directories.addAll(directories);
+    }
+
+    @Override
+    public List<LocalFile> getFiles() {
+        return this.files;
+    }
+
+    @Override
+    public List<LocalDirectory> getDirectories() {
+        return this.directories;
+    }
 }
--- app/src/main/java/org/example/core/repository/remote/RemoteCollection.java
+++ app/src/main/java/org/example/core/repository/remote/RemoteCollection.java
@@ -1,6 +1,30 @@
 package org.example.core.repository.remote;
 
+import org.example.core.repository.RepositoryCollection;
+import org.example.core.repository.RepositoryDirectory;
+import org.example.core.repository.RepositoryFile;
+
+import java.net.URL;
 import java.util.List;
 
-public record RemoteCollection(List<RemoteFile> files, List<RemoteDirectory> directories) {
+public record RemoteCollection(List<RemoteFile> files, List<RemoteDirectory> directories) implements RepositoryCollection<URL, RemoteDirectory, RemoteFile> {
+    @Override
+    public void setFiles(List<RemoteFile> files) {
+        this.files.addAll(files);
+    }
+
+    @Override
+    public void setDirectories(List<RemoteDirectory> directories) {
+        this.directories.addAll(directories);
+    }
+
+    @Override
+    public List<RemoteFile> getFiles() {
+        return this.files;
+    }
+
+    @Override
+    public List<RemoteDirectory> getDirectories() {
+        return this.directories;
+    }
 }
