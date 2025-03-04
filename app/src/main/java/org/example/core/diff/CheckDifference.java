d1a25784ca75e9aeebbbb2026243e0613c160da7
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 14:07:11 +0100

    feat: init difference checking algorithm

--- app/src/main/java/org/example/core/diff/CheckDifference.java
+++ app/src/main/java/org/example/core/diff/CheckDifference.java
@@ -1,2 +1,10 @@
-package org.example.core.diff;public class CheckDifference {
+package org.example.core.diff;
+
+import java.io.File;
+import java.util.List;
+
+public class CheckDifference {
+    public static List<File> checkDifference(final String user, final String repo, final String accessToken, final String localPath, final String remotePath) {
+        return null;
+    }
 }
