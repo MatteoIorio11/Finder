358a11ee86d6f681665144d1c832e3e6295730d9
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 14:28:25 +0100

    feat: make the two Generics extends from the correct Repository Component

--- app/src/main/java/org/example/core/repository/Repository.java
+++ app/src/main/java/org/example/core/repository/Repository.java
@@ -1,11 +1,8 @@
 package org.example.core.repository;
 
-import org.example.core.remote.RemoteDirectory;
-import org.example.core.remote.RemoteFile;
-
 import java.util.List;
 
-public interface Repository<X, Y>{
+public interface Repository<X extends RepositoryDirectory, Y extends RepositoryFile>{
     void addDirectory(X directory);
     void addFile(Y file);
     List<X> getDirectories();
