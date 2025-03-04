2f46ea2a69b393da7a81d0273dcea99bfb7f14ac
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 17:42:52 +0100

    feat: init local scraper

--- app/src/main/java/org/example/core/scraper/local/LocalScraperImpl.java
+++ app/src/main/java/org/example/core/scraper/local/LocalScraperImpl.java
@@ -1,5 +1,7 @@
 package org.example.core.scraper.local;
 
+import org.example.core.repository.RepositoryCollection;
+import org.example.core.repository.local.LocalCollection;
 import org.example.core.repository.local.LocalDirectory;
 import org.example.core.repository.local.LocalFile;
 import org.example.core.repository.local.LocalRepository;
@@ -8,10 +10,16 @@ import org.example.core.scraper.AbstractScraper;
 import java.nio.file.Path;
 import java.util.Optional;
 
-public class LocalScraperImpl extends AbstractScraper<Path, LocalDirectory, LocalFile, LocalRepository> {
+public class LocalScraperImpl extends AbstractScraper<Path, LocalDirectory, LocalFile, LocalRepository, LocalCollection> {
 
     @Override
-    public LocalRepository getRepository(final Path repositoryUrl, final Optional<String> inputToken) {
+    public LocalRepository getRepository(final Path repositoryPath, final Optional<String> inputToken) {
         return null;
     }
+
+    @Override
+    protected Optional<LocalCollection> readFromPath(Path path, Optional<String> token) {
+        return Optional.empty();
+    }
+
 }
