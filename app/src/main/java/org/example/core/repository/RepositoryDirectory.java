098c987c3c2d5876f0f31d21d3a8b0488cf9975d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 15:06:41 +0100

    feat: add new Repository components

--- app/src/main/java/org/example/core/repository/RepositoryDirectory.java
+++ app/src/main/java/org/example/core/repository/RepositoryDirectory.java
@@ -1,4 +1,10 @@
 package org.example.core.repository;
 
-public interface RepositoryDirectory extends RepositoryElement {
+import java.util.List;
+
+public interface RepositoryDirectory<Y extends RepositoryFile> extends RepositoryElement{
+    List<Y> getFiles();
+    List<RepositoryDirectory<Y>> getInnerDirectories();
+    void addFile(Y file);
+    void addDirectory(RepositoryDirectory<Y> directory);
 }
--- app/src/main/java/org/example/core/repository/RepositoryFile.java
+++ app/src/main/java/org/example/core/repository/RepositoryFile.java
@@ -1,4 +1,5 @@
 package org.example.core.repository;
 
 public interface RepositoryFile extends RepositoryElement{
+    String getContent();
 }
