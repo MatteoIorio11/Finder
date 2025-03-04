039014a23543809944757de8f9112e738bc14719
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-04 16:12:48 +0100

    feat: add new generic for Path type

--- app/src/main/java/org/example/core/repository/Repository.java
+++ app/src/main/java/org/example/core/repository/Repository.java
@@ -2,7 +2,7 @@ package org.example.core.repository;
 
 import java.util.List;
 
-public interface Repository<X extends RepositoryDirectory<Y>, Y extends RepositoryFile>{
+public interface Repository<P, X extends RepositoryDirectory<P, Y>, Y extends RepositoryFile<P>>{
     void addDirectory(X directory);
     void addFile(Y file);
     List<X> getDirectories();
