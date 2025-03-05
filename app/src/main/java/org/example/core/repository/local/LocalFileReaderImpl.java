f496697f62e2e068a1a394f43f994298396bb63d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-05 11:55:20 +0100

    feat: create new local file reader

---
+++ app/src/main/java/org/example/core/repository/local/LocalFileReaderImpl.java
@@ -0,0 +1,19 @@
+package org.example.core.repository.local;
+
+import org.example.core.repository.AbstractFileReader;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class LocalFileReaderImpl extends AbstractFileReader<Path> {
+
+    @Override
+    public String getContent(Path path, Optional<String> token) {
+        try {
+            return Files.readString(path);
+        }catch (IOException ignored) {}
+        return "";
+    }
+}
