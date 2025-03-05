87ea5eb267d5d33d429560f418dd1efaf06a5d03
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-05 09:56:18 +0100

    refactor: create AbstractRepositoryDirectory instead of RepositoryDirectory interface

--- app/src/main/java/org/example/core/repository/AbstractRepository.java
+++ app/src/main/java/org/example/core/repository/AbstractRepository.java
@@ -1,13 +1,19 @@
 package org.example.core.repository;
 
+import java.net.URL;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-public abstract class AbstractRepository<P, X extends RepositoryDirectory<P, Y>, Y extends RepositoryFile<P>>{
+public abstract class AbstractRepository<P, X extends AbstractRepositoryDirectory<P, Y>, Y extends RepositoryFile<P>>{
     private final List<X> directories;
     private final List<Y> files;
-    protected AbstractRepository() {
+    private final String name;
+    private final URL remoteUrl;
+
+    protected AbstractRepository(final String name, final URL remoteUrl) {
+        this.name = name;
+        this.remoteUrl = remoteUrl;
         this.directories = new LinkedList<>();
         this.files = new LinkedList<>();
     }
---
+++ app/src/main/java/org/example/core/repository/AbstractRepositoryDirectory.java
@@ -0,0 +1,52 @@
+package org.example.core.repository;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractRepositoryDirectory<P, Y extends RepositoryFile<P>> implements RepositoryElement<P> {
+    private final List<AbstractRepositoryDirectory<P, Y>> directories;
+    private final List<Y> files;
+    private final String name;
+    private final P remoteUrl;
+
+    public AbstractRepositoryDirectory(final String name, final P remoteUrl) {
+        this.directories = new LinkedList<>();
+        this.files = new LinkedList<>();
+        this.name = Objects.requireNonNull(name);
+        this.remoteUrl = Objects.requireNonNull(remoteUrl);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public P getPath() {
+        return remoteUrl;
+    }
+
+    public List<Y> getFiles() {
+        return Collections.unmodifiableList(this.files);
+    }
+    public List<AbstractRepositoryDirectory<P, Y>> getInnerDirectories(){
+        return Collections.unmodifiableList(this.directories);
+    }
+    public void addFile(Y file) {
+        this.files.add(file);
+    }
+    public void addDirectory(AbstractRepositoryDirectory<P, Y> directory) {
+        this.directories.add(directory);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteDirectoryImpl{" +
+                "name='" + name + '\'' +
+                ", remoteUrl='" + remoteUrl + '\'' +
+                '}';
+    }
+}
