dffae5acbfb3b170f4e88494be63010a69741ac1
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-06 15:39:17 +0100

    doc: add javadoc for repository collection

--- app/src/main/java/org/example/core/repository/RepositoryCollection.java
+++ app/src/main/java/org/example/core/repository/RepositoryCollection.java
@@ -2,9 +2,21 @@ package org.example.core.repository;
 
 import java.util.List;
 
+/**
+ * A collection of files and directories
+ * @param <P> the type of the path
+ * @param <X> the type of the directory
+ * @param <Y> the type of the file
+ */
 public interface RepositoryCollection<P, X extends AbstractRepositoryDirectory<P, Y>, Y extends AbstractRepositoryFile<P>> {
-    void setFiles(List<Y> files);
-    void setDirectories(List<X> directories);
+    /**
+     * Get the files in the collection
+     * @return the files in the collection
+     */
     List<Y> getFiles();
+    /**
+     * Get the directories in the collection
+     * @return the directories in the collection
+     */
     List<X> getDirectories();
 }
