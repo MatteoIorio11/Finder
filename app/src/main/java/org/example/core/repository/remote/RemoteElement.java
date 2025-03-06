e425fcf0627e0b02a80b52eee0bde811339781c0
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-06 15:46:40 +0100

    doc: add javadoc to local and remote element

--- app/src/main/java/org/example/core/repository/remote/RemoteElement.java
+++ app/src/main/java/org/example/core/repository/remote/RemoteElement.java
@@ -4,5 +4,8 @@ import org.example.core.repository.RepositoryElement;
 
 import java.net.URL;
 
+/**
+ * A remote element in a repository.
+ */
 public interface RemoteElement extends RepositoryElement<URL> {
 }
