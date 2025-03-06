23101910a9a1f5833951784ed81eb21c9c8584b0
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-06 15:42:23 +0100

    doc: add javadoc to AbstractScraper

--- app/src/main/java/org/example/core/scraper/AbstractScraper.java
+++ app/src/main/java/org/example/core/scraper/AbstractScraper.java
@@ -1,5 +1,6 @@
 package org.example.core.scraper;
 
+import org.checkerframework.checker.units.qual.A;
 import org.example.core.repository.AbstractRepository;
 import org.example.core.repository.RepositoryCollection;
 import org.example.core.repository.AbstractRepositoryDirectory;
@@ -11,13 +12,37 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * An abstract scraper
+ * @param <P> the type of the path
+ * @param <X> the type of the directory
+ * @param <Y> the type of the file
+ * @param <Z> the type of the collection
+ */
 public abstract class AbstractScraper<P, X extends AbstractRepositoryDirectory<P, Y>, Y extends AbstractRepositoryFile<P>, Z extends RepositoryCollection<P, X, Y>> {
     private final boolean needSleep;
+    /**
+     * Constructor for AbstractScraper
+     * @param needSleep whether the scraper needs to sleep
+     */
     protected AbstractScraper(final boolean needSleep) {
         this.needSleep = needSleep;
     }
+    /**
+     * Get the repository
+     * @param repositoryName the name of the repository
+     * @param repositoryPath the path of the repository
+     * @param inputToken the input token
+     * @return the repository
+     */
     abstract public AbstractRepository<P, X, Y> getRepository(final String repositoryName, final P repositoryPath, final Optional<String> inputToken);
 
+    /**
+     * Build the repository recursively using the readFromPath method
+     * @param path the path to build the repository from
+     * @param repository the repository to build
+     * @param inputToken the input token
+     */
     protected void buildRepository(final P path, final AbstractRepository<P, X, Y> repository, final Optional<String> inputToken) {
         final Set<String> seen = new HashSet<>();
         this.readFromPath(repository.getName(), path, inputToken).ifPresent(collection -> {
@@ -28,6 +53,14 @@ public abstract class AbstractScraper<P, X extends AbstractRepositoryDirectory<P
             });
         });
     }
+
+    /**
+     * Build a directory recursively using the readFromPath method
+     * @param repositoryName the name of the repository
+     * @param directory the directory to build
+     * @param token the token
+     * @param seen the set of seen paths
+     */
     protected void buildDirectory(@NotNull final String repositoryName, @NotNull final AbstractRepositoryDirectory<P, Y> directory, @NotNull final Optional<String> token, @NotNull final Set<String> seen) {
         if (seen.contains(directory.getPath().toString()) || Objects.isNull(directory.getPath())) {
             return;
@@ -43,11 +76,21 @@ public abstract class AbstractScraper<P, X extends AbstractRepositoryDirectory<P
         });
     }
 
+    /**
+     * Sleep for 2 seconds
+     */
     protected void sleep() {
         try{
             Thread.sleep(2000);
         } catch (InterruptedException ignored) {}
     }
 
+    /**
+     * Read from a path (the path could be local or remote)
+     * @param repositoryName the name of the repository
+     * @param path the path to read from
+     * @param token the token
+     * @return the collection
+     */
     protected abstract Optional<Z> readFromPath(final String repositoryName, final P path, final Optional<String> token);
 }
