9d1e7e63d5dd80b54a8201dcdb36cf76815949fc
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-06 15:38:57 +0100

    doc: add javadoc for abstract repository directory

--- app/src/main/java/org/example/core/repository/AbstractRepositoryDirectory.java
+++ app/src/main/java/org/example/core/repository/AbstractRepositoryDirectory.java
@@ -1,13 +1,25 @@
 package org.example.core.repository;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
+/**
+ * An abstract class for a directory in a repository
+ * @param <P> the type of the path
+ * @param <Y> the type of the file
+ */
 public abstract class AbstractRepositoryDirectory<P, Y extends AbstractRepositoryFile<P>> implements RepositoryElement<P> {
     private final Map<String, AbstractRepositoryDirectory<P, Y>> directories;
     private final Map<String, Y> files;
     private final String name;
     private final P remoteUrl;
 
+    /**
+     * Constructor for AbstractRepositoryDirectory
+     * @param name the name of the directory
+     * @param remoteUrl the remote url of the directory
+     */
     public AbstractRepositoryDirectory(final String name, final P remoteUrl) {
         this.directories = new HashMap<>();
         this.files = new HashMap<>();
@@ -25,31 +37,67 @@ public abstract class AbstractRepositoryDirectory<P, Y extends AbstractRepositor
         return remoteUrl;
     }
 
+    /**
+     * Get the files in the directory
+     * @return the files in the directory
+     */
     public List<Y> getFiles() {
         return this.files.values().stream().toList();
     }
+    /**
+     * Get the directories in the directory
+     * @return the directories in the directory
+     */
     public List<AbstractRepositoryDirectory<P, Y>> getInnerDirectories(){
         return this.directories.values().stream().toList();
     }
-    public void addFile(Y file) {
+    /**
+     * Add a file to the directory
+     * @param file the file to add
+     */
+    public void addFile(@NotNull final Y file) {
         this.files.put(file.getName(), file);
     }
-    public void addDirectory(AbstractRepositoryDirectory<P, Y> directory) {
+    /**
+     * Add a directory to the directory
+     * @param directory the directory to add
+     */
+    public void addDirectory(@NotNull final AbstractRepositoryDirectory<P, Y> directory) {
         this.directories.put(directory.getName(), directory);
     }
 
-    public boolean hasFile(final String name) {
+    /**
+     * Check if the directory has a file
+     * @param name the name of the file
+     * @return true if the directory has the file, false otherwise
+     */
+    public boolean hasFile(@NotNull final String name) {
         return this.files.containsKey(name);
     }
-    public boolean hasDirectory(final String name) {
+    /**
+     * Check if the directory has a directory
+     * @param name the name of the directory
+     * @return true if the directory has the directory, false otherwise
+     */
+    public boolean hasDirectory(@NotNull final String name) {
         return this.directories.containsKey(name);
     }
 
-    public Optional<Y> getFile(final String name) {
+    /**
+     * Get the file with the given name
+     * @param name the name of the file
+     * @return the file with the given name
+     */
+    public Optional<Y> getFile(@NotNull final String name) {
         return Optional.ofNullable(this.files.get(name));
     }
 
-    public Optional<AbstractRepositoryDirectory<P, Y>> getDirectory(final String name) {
+    /**
+     * Get the directory with the given name
+     * @param name the name of the directory
+     * @return the directory with the given name
+     */
+    public Optional<AbstractRepositoryDirectory<P, Y>> getDirectory(@NotNull final String name) {
         return Optional.ofNullable(this.directories.get(name));
     }
 
