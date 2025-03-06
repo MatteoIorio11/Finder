a8aaccdee5f0b0790a230b7a0c23fc549ed6e904
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-06 15:31:15 +0100

    feat: implement all the different algorithms for automatically check the common directories and the common files. Add logic for checking if the content of two files is different

--- app/src/main/java/org/example/core/diff/CheckDifference.java
+++ app/src/main/java/org/example/core/diff/CheckDifference.java
@@ -11,52 +11,79 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class CheckDifference {
     private final static String GITHUB_URL = "https://github.com";
-    public static List<String> checkDifference(final String user, final String repo, final String accessToken, final String localPath, final String branchA, final String branchB) throws MalformedURLException {
+    public static List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>>  checkDifference(final String user, final String repo, final String accessToken, final String localPath, final String branchA, final String branchB) throws MalformedURLException {
         final URL url = URI.create(GITHUB_URL + "/" + user + "/" + repo + "/tree/" + branchA).toURL();
-        System.out.println(url);
         BranchSwitcher.switchBranch(localPath, branchB);
         final AbstractRepository<URL, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>, AbstractRepositoryFile<URL>> remoteRepository = RepositoryFactory.remoteRepository(repo, url, accessToken);
         final AbstractRepository<Path, AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryFile<Path>> localRepository = RepositoryFactory.localRepository(repo, Path.of(localPath));
-        System.out.println(checkCommonDirectoriesFromRepository(localRepository, remoteRepository));
-        return List.of();
+        final List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>>  output = new LinkedList<>(checkForDifferences(commonFilesFromRepository(localRepository, remoteRepository)));
+        checkCommonDirectoriesFromRepository(localRepository, remoteRepository)
+                .forEach(entry -> {
+                    output.addAll(recursiveCheck(checkCommonDirectories(entry.getKey(), entry.getValue())));
+                });
+        return output;
     }
+
+    private static List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>>  recursiveCheck(final List<Map.Entry<AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>>> commonDirectories){
+        final List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> differences = new LinkedList<>();
+        commonDirectories
+                .forEach(entry -> {
+                    final List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> commonFiles = commonFilesFromDirectory(entry.getKey(), entry.getValue());
+                    differences.addAll(checkForDifferences(commonFiles));
+                    differences.addAll(recursiveCheck(checkCommonDirectories(entry.getKey(), entry.getValue())));
+                });
+        return differences;
+    }
+
+
 
     private static List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> commonFilesFromRepository(final AbstractRepository<Path, AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryFile<Path>> localRepository, final AbstractRepository<URL, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>, AbstractRepositoryFile<URL>> remoteRepository) {
         return localRepository.getFiles().stream()
                 .filter(file -> remoteRepository.hasFile(file.getName()))
-                .map(file -> Map.entry(file, remoteRepository.getFile(file.getName()).get()))
+                .map(file -> Map.entry(file, remoteRepository.getFile(file.getName())))
+                .filter(entry -> entry.getValue().isPresent())
+                .map(entry -> Map.entry(entry.getKey(), entry.getValue().get()))
                 .toList();
     }
 
     private static List<Map.Entry<AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>>> checkCommonDirectoriesFromRepository(final AbstractRepository<Path, AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryFile<Path>> localRepository, final AbstractRepository<URL, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>, AbstractRepositoryFile<URL>> remoteRepository) {
         return localRepository.getDirectories().stream()
                 .filter(directory -> remoteRepository.hasDirectory(directory.getName()))
-                .map(directory -> Map.entry(directory, remoteRepository.getDirectory(directory.getName()).get()))
+                .map(directory -> Map.entry(directory, remoteRepository.getDirectory(directory.getName())))
+                .filter(entry -> entry.getValue().isPresent())
+                .map(entry -> Map.entry(entry.getKey(), entry.getValue().get()))
                 .toList();
     }
 
     private static List<Map.Entry<AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>>> checkCommonDirectories(final AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>> localDirectory, final AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>> remoteDirectory) {
         return localDirectory.getInnerDirectories().stream()
                 .filter(directory -> remoteDirectory.hasDirectory(directory.getName()))
-                .map(directory -> Map.entry(directory, remoteDirectory.getDirectory(directory.getName()).get()))
+                .map(directory -> Map.entry(directory, remoteDirectory.getDirectory(directory.getName())))
+                .filter(entry -> entry.getValue().isPresent())
+                .map(entry -> Map.entry(entry.getKey(), entry.getValue().get()))
                 .toList();
     }
 
     private static List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> commonFilesFromDirectory(final AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>> localDir, final AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>> remoteDir) {
         return localDir.getFiles().stream()
                 .filter(file -> remoteDir.hasFile(file.getName()))
-                .map(file -> Map.entry(file, remoteDir.getFile(file.getName()).get()))
+                .map(file -> Map.entry(file, remoteDir.getFile(file.getName())))
+                .filter(entry -> entry.getValue().isPresent())
+                .map(entry -> Map.entry(entry.getKey(), entry.getValue().get()))
                 .toList();
     }
 
-    private static List<String> checkForDifferences(final List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> commonFiles) {
-        return commonFiles.stream().filter(commonFile -> commonFile.getKey().getContent(new LocalFileReaderImpl()).equals(commonFile.getValue().getContent(new RemoteFileReaderImpl())))
-                .map(commonFile -> commonFile.getKey().getName())
+    private static List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> checkForDifferences(final List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> commonFiles) {
+        final var localReader = new LocalFileReaderImpl();
+        final var remoteReader = new RemoteFileReaderImpl();
+        return commonFiles.stream()
+                .filter(commonFile -> !commonFile.getKey().getContent(localReader).equals(commonFile.getValue().getContent(remoteReader)))
                 .toList();
     }
 }
