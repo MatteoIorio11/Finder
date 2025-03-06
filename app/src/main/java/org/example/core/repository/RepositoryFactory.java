e8ebce6f5dd919c59061ab3393c08f6c3ddf4e39
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-06 12:02:04 +0100

    feat: add repository name

--- app/src/main/java/org/example/core/repository/RepositoryFactory.java
+++ app/src/main/java/org/example/core/repository/RepositoryFactory.java
@@ -1,5 +1,7 @@
 package org.example.core.repository;
 
+import org.example.core.repository.local.LocalRepositoryImpl;
+import org.example.core.scraper.local.LocalScraperImpl;
 import org.example.core.scraper.remote.RemoteScraperImpl;
 
 import java.net.URL;
@@ -8,12 +10,11 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class RepositoryFactory {
-    public static AbstractRepository<URL, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>, AbstractRepositoryFile<URL>> remoteRepository(final URL repositoryUrl, final String token) {
-        return new RemoteScraperImpl().getRepository(Objects.requireNonNull(repositoryUrl), Optional.of(Objects.requireNonNull(token)));
+    public static AbstractRepository<URL, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>, AbstractRepositoryFile<URL>> remoteRepository(final String repoName, final URL repositoryUrl, final String token) {
+        return new RemoteScraperImpl().getRepository(repoName, Objects.requireNonNull(repositoryUrl), Optional.of(Objects.requireNonNull(token)));
     }
 
-    public static void localRepository(final Path repositoryPath) {
-//        return HttpAgent.getRemoteRepository(Objects.requireNonNull(repositoryUrl));
-        return;
+    public static AbstractRepository<Path, AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryFile<Path>> localRepository(final String repoName, final Path repositoryPath) {
+        return new LocalScraperImpl().getRepository(repoName, repositoryPath, Optional.empty());
     }
 }
