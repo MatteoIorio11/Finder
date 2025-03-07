3677e060c360e475af748135eb08512da52879be
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-07 08:52:58 +0100

    test: init secret configurator tests

--- app/src/test/java/org/example/AbstractSecretConfigurator.java
+++ app/src/test/java/org/example/AbstractSecretConfigurator.java
@@ -1,2 +1,14 @@
-package org.example;public class AbstractSecretConfigurator {
+package org.example;
+
+import org.example.core.configuration.SecretConfigurator;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+public class AbstractSecretConfigurator {
+    @BeforeAll
+    private void init() {
+        SecretConfigurator.readSecrets("test/main/resources", ".env");
+    }
+
+    
 }
