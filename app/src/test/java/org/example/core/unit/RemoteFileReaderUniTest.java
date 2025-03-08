8a871c61fa860a015951bc159cbe5ba7c5b2ea1f
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-08 18:19:52 +0100

    test: add RemoteFileReader test

---
+++ app/src/test/java/org/example/core/unit/RemoteFileReaderUniTest.java
@@ -0,0 +1,26 @@
+package org.example.core.unit;
+
+import jdk.jfr.Description;
+import org.example.core.repository.AbstractFileReader;
+import org.example.core.repository.remote.RemoteFileReaderImpl;
+import org.junit.Test;
+import org.junit.jupiter.api.Tag;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RemoteFileReaderUniTest {
+    private final AbstractFileReader<URL> reader = new RemoteFileReaderImpl();
+
+    @Description("If the URL is valid, then It should be possible to read the Its content")
+    @Test
+    @Tag("unit")
+    public void testContentOfExistingFileShouldExist() throws MalformedURLException {
+        final String content = reader.getContent(URI.create("https://github.com/MatteoIorio11/MatteoIorio11/blob/main/README.md").toURL());
+        assertNotNull(content);
+        assertFalse(content.isEmpty());
+    }
+}
