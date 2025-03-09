ef844b9a7b135ddb0f964780a08df4da53b56efa
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-09 10:53:32 +0100

    refactor: make returned lists as unmodifiable

--- app/src/main/java/org/example/core/repository/remote/RemoteCollection.java
+++ app/src/main/java/org/example/core/repository/remote/RemoteCollection.java
@@ -5,15 +5,16 @@ import org.example.core.repository.AbstractRepositoryFile;
 import org.example.core.repository.RepositoryCollection;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 
 public record RemoteCollection(List<AbstractRepositoryFile<URL>> files, List<AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>> directories) implements RepositoryCollection<URL, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>, AbstractRepositoryFile<URL>> {
     @Override
     public List<AbstractRepositoryFile<URL>> getFiles() {
-        return this.files;
+        return Collections.unmodifiableList(this.files);
     }
     @Override
     public List<AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>> getDirectories() {
-        return this.directories;
+        return Collections.unmodifiableList(this.directories);
     }
 }
