ec5a9acf1d20128544282d3b4711298018dcee6c
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-09 18:06:57 +0100

    refactor: remove the input directory from the secret reader

--- app/src/main/java/org/example/core/configuration/SecretConfigurator.java
+++ app/src/main/java/org/example/core/configuration/SecretConfigurator.java
@@ -3,13 +3,13 @@ package org.example.core.configuration;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class SecretConfigurator {
-    public static void readSecrets(final String secretPath, final String fileName) {
-        final Dotenv dotenv = Dotenv.configure().directory(secretPath).filename(fileName).load();
+    public static void readSecrets(final String fileName) {
+        final Dotenv dotenv = Dotenv.configure().filename(fileName).load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
     }
 
     public static void readTestSecrets() {
-        readSecrets("src/test/resources", ".env");
+        readSecrets(".env");
     }
 
 }
--- app/src/test/java/org/example/core/unit/secret/SecretConfiguratorUnitTest.java
+++ app/src/test/java/org/example/core/unit/secret/SecretConfiguratorUnitTest.java
@@ -13,21 +13,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class SecretConfiguratorUnitTest {
     @AfterAll
     public static void setProperties() {
-        SecretConfigurator.readSecrets("src/test/resources", ".env");
+        SecretConfigurator.readSecrets(".env");
     }
     @Description("If the file does not exist, the application should throw an exception")
     @Test
     @Tag("unit")
     public void testTryReadSecretsWithoutFile() {
-        assertThrows(DotenvException.class,() -> SecretConfigurator.readSecrets("src/NOT_A_DIRECTORY", ".env"));
+        assertThrows(DotenvException.class,() -> SecretConfigurator.readSecrets(".notafile"));
     }
 
     @Description("If the file exists, then It should be possible to read the secrets")
     @Test
     @Tag("unit")
     public void testCheckIfSecretsAreLoaded() {
-        SecretConfigurator.readSecrets("src/test/resources", ".env");
+        SecretConfigurator.readSecrets( ".env");
         assertFalse(System.getProperties().isEmpty());
-        assertNotNull(System.getProperty("TEST"));
+        assertNotNull(System.getProperty("GITHUB_TOKEN"));
     }
 }
