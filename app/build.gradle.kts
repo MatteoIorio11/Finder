a72167a292fad5e0b8a5d4118000595158b9c8b9
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-10 16:03:41 +0100

    chore: add new task for automatically clone and move the repository test. This task is set as a dependency for the task -> test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@
  * This generated file contains a sample Java application project to get you started.
  * For more details on building Java & JVM projects, please refer to https://docs.gradle.org/8.12.1/userguide/building_java_projects.html in the Gradle documentation.
  */
+import java.nio.file.Files
+import java.nio.file.Paths
 
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
@@ -38,7 +40,38 @@ java {
     }
 }
 
+tasks.register("cloneTestRepository") {
+    doLast {
+        val homeDir = System.getProperty("user.home")
+        val repoDir = Paths.get(homeDir, "FinderTest")
+
+        if (Files.exists(repoDir)) {
+            println("Repository already exists at $repoDir. Skipping clone.")
+            return@doLast
+        }
+
+        val isWindows = System.getProperty("os.name").startsWith("Windows")
+
+        val command = if (isWindows) {
+            listOf("cmd", "/c", "git clone git@github.com:MatteoIorio11/FinderTest.git && move FinderTest \"$homeDir\"")
+        } else {
+            listOf("sh", "-c", "git clone git@github.com:MatteoIorio11/FinderTest.git && mv FinderTest \"$homeDir\"")
+        }
+
+        val process = ProcessBuilder(command)
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .start()
+
+        val exitCode = process.waitFor()
+        if (exitCode != 0) {
+            throw RuntimeException("Shell command failed with exit code $exitCode")
+        }
+    }
+}
+
 tasks.test {
+    dependsOn("cloneTestRepository")
     useJUnitPlatform()
 }
 
---
+++ app/src/test/java/org/iorio/core/integration/repository/factory/RepositoryElements.java
@@ -0,0 +1,2 @@
+package org.iorio.core.integration.repository.factory;public class RepositoryElements {
+}
