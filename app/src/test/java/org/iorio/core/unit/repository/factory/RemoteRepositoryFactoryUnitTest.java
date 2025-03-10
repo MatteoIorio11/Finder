e10a1945e97d494bd58e067c368ed38c67f3a29b
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-10 15:05:20 +0100

    test: test the repository factor with a Null token, test the repository factory with an invalid URL

--- app/src/test/java/org/iorio/core/unit/repository/factory/RemoteRepositoryFactoryUnitTest.java
+++ app/src/test/java/org/iorio/core/unit/repository/factory/RemoteRepositoryFactoryUnitTest.java
@@ -5,6 +5,9 @@ import org.iorio.core.repository.RepositoryFactory;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoteRepositoryFactoryUnitTest {
@@ -15,5 +18,21 @@ public class RemoteRepositoryFactoryUnitTest {
         assertThrows(IllegalArgumentException.class, () -> RepositoryFactory.remoteRepository("repoName", null, "token"));
     }
 
-    
+    @Description("If the repository token is null, an IllegalArgumentException should be thrown")
+    @Test
+    @Tag("unit")
+    public void testRemoteRepositoryWithNullToken() {
+        assertThrows(IllegalArgumentException.class,
+                () -> RepositoryFactory.remoteRepository("repoName", URI.create("https://internship.jetbrains.com/admissions").toURL(), null));
+    }
+
+    @Description("If the repository URL does not exists, an IllegalArgumentException should be thrown")
+    @Test
+    @Tag("unit")
+    public void testRemoteRepositoryWithInvalidUrl() {
+        assertThrows(IllegalArgumentException.class,
+                () -> RepositoryFactory.remoteRepository("repoName", URI.create("https://matteoiorioaaaa.eu").toURL(), "token"));
+    }
+
+
 }
