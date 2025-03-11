a9abacfc34e072dfccd0b3bcf4228e7ef22978ac
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-11 16:10:16 +0100

    feat: add constructor inside local collection. Check for non null arguments

--- app/src/main/java/org/iorio/core/repository/local/LocalCollection.java
+++ app/src/main/java/org/iorio/core/repository/local/LocalCollection.java
@@ -6,8 +6,13 @@ import org.iorio.core.repository.RepositoryCollection;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 
 public record LocalCollection(List<AbstractRepositoryFile<Path>> files, List<AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>> directories) implements RepositoryCollection<Path, AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>, AbstractRepositoryFile<Path>> {
+    public LocalCollection(final List<AbstractRepositoryFile<Path>> files, final List<AbstractRepositoryDirectory<Path, AbstractRepositoryFile<Path>>> directories) {
+        this.files = Objects.requireNonNull(files);
+        this.directories = Objects.requireNonNull(directories);
+    }
     @Override
     public List<AbstractRepositoryFile<Path>> getFiles() {
         return this.files;
