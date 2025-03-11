f6efcace1f31ac1d11464f2a964f3c05cc10fc68
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-11 12:48:58 +0100

    test: add test implementation for remote directory

--- app/src/test/java/org/iorio/core/integration/repository/directory/RemoteDirectoryIntegrationTest.java
+++ app/src/test/java/org/iorio/core/integration/repository/directory/RemoteDirectoryIntegrationTest.java
@@ -1,2 +1,36 @@
-package org.iorio.core.integration.repository.directory;public class RemoteDirectoryIntegrationTest {
+package org.iorio.core.integration.repository.directory;
+
+import org.iorio.core.configuration.SecretConfigurator;
+import org.iorio.core.repository.AbstractRepositoryDirectory;
+import org.iorio.core.repository.AbstractRepositoryFile;
+import org.iorio.core.repository.remote.RemoteDirectoryImpl;
+import org.iorio.core.repository.remote.RemoteFileImpl;
+import org.iorio.core.repository.remote.RemoteFileReaderImpl;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class RemoteDirectoryIntegrationTest extends AbstractRepositoryDirectoryIntegrationTest<URL, AbstractRepositoryFile<URL>> {
+    private static AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>> remoteDirectory;
+    public RemoteDirectoryIntegrationTest() {
+        super(
+                remoteDirectory,
+                new RemoteFileReaderImpl()
+        );
+    }
+
+    @BeforeAll
+    public static void setUp() throws MalformedURLException {
+        SecretConfigurator.readTestSecrets();
+        remoteDirectory = new RemoteDirectoryImpl(
+                "FinderTest/dir1",
+                URI.create("https://github.com/MatteoIorio11/FinderTest/tree/main/dir1").toURL()
+        );
+        remoteDirectory.addFile(new RemoteFileImpl(
+                "FinderTest/dir1/file3",
+                URI.create("https://github.com/MatteoIorio11/FinderTest/blob/main/dir1/file3").toURL()));
+
+    }
 }
