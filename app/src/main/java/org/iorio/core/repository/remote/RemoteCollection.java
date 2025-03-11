90b7f51f1a096f550f169bb322de0da946fbeaf8
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-11 16:09:57 +0100

    feat: add constructor inside remote collection. Check for non null arguments

--- app/src/main/java/org/iorio/core/repository/remote/RemoteCollection.java
+++ app/src/main/java/org/iorio/core/repository/remote/RemoteCollection.java
@@ -7,8 +7,13 @@ import org.iorio.core.repository.RepositoryCollection;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public record RemoteCollection(List<AbstractRepositoryFile<URL>> files, List<AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>> directories) implements RepositoryCollection<URL, AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>, AbstractRepositoryFile<URL>> {
+    public RemoteCollection(final List<AbstractRepositoryFile<URL>> files, final List<AbstractRepositoryDirectory<URL, AbstractRepositoryFile<URL>>> directories) {
+        this.files = Objects.requireNonNull(files);
+        this.directories = Objects.requireNonNull(directories);
+    }
     @Override
     public List<AbstractRepositoryFile<URL>> getFiles() {
         return Collections.unmodifiableList(this.files);
