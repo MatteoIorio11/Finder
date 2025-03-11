8de8875872b480ab178105f5f89e9020a8c52f93
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-11 12:44:29 +0100

    test(integration): add new directory integration tests

---
+++ app/src/test/java/org/iorio/core/integration/repository/directory/AbstractRepositoryDirectoryIntegrationTest.java
@@ -0,0 +1,28 @@
+package org.iorio.core.integration.repository.directory;
+
+import jdk.jfr.Description;
+import org.iorio.core.repository.AbstractFileReader;
+import org.iorio.core.repository.AbstractRepositoryDirectory;
+import org.iorio.core.repository.AbstractRepositoryFile;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public abstract class AbstractRepositoryDirectoryIntegrationTest<P, Y extends AbstractRepositoryFile<P>> {
+    private final AbstractRepositoryDirectory<P, Y> directory;
+    private final AbstractFileReader<P> reader;
+    protected AbstractRepositoryDirectoryIntegrationTest(final AbstractRepositoryDirectory<P, Y> directory,
+                                                         final AbstractFileReader<P> reader) {
+        this.directory = directory;
+        this.reader = reader;
+    }
+
+    @Description("If the directory is valid, then It should be possible to read the content of the files")
+    @Test
+    @Tag("integration")
+    public void testReadContent() {
+        final var content = this.directory.getFile("FinderTest/dir1/file3");
+        assertTrue(content.get().getContent(this.reader).contains("Test"));
+    }
+}
