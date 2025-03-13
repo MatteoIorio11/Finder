5e193844496af1ea9e75e22cd0017d0af2ef7119
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-13 10:48:31 +0100

    test(unit): use mockito for unit testing

--- app/src/test/java/org/iorio/core/unit/repository/file/LocalFileUnitTest.java
+++ app/src/test/java/org/iorio/core/unit/repository/file/LocalFileUnitTest.java
@@ -1,6 +1,7 @@
 package org.iorio.core.unit.repository.file;
 
 import jdk.jfr.Description;
+import org.iorio.core.repository.AbstractFileReader;
 import org.iorio.core.repository.AbstractRepositoryFile;
 import org.iorio.core.repository.local.LocalFileImpl;
 import org.iorio.core.repository.local.LocalFileReaderImpl;
@@ -8,22 +9,26 @@ import org.iorio.core.unit.repository.AbstractRepositoryElementUnitTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-public class FileUnitTest extends AbstractRepositoryElementUnitTest<Path> {
+public class LocalFileUnitTest extends AbstractRepositoryElementUnitTest<Path> {
     private static AbstractRepositoryFile<Path> file;
-    public FileUnitTest() {
+    private static LocalFileReaderImpl reader;
+    public LocalFileUnitTest() {
         super(file);
     }
 
     @BeforeAll
     public static void init() {
-
+        reader = Mockito.mock(LocalFileReaderImpl.class);
         file = new LocalFileImpl("FileTest", Path.of("src/test/resources/.env"));
+        when(reader.getContent(file.getPath())).thenReturn(List.of("TEST"));
     }
 
     @Description("If the file does not exist, then It should throw an exception")
@@ -32,5 +37,14 @@ public class FileUnitTest extends AbstractRepositoryElementUnitTest<Path> {
     public void testContentOfNonExistingFileShouldThrowException() {
         final var nonExistingFile = new LocalFileImpl("NonExistingFile", Path.of("src/INVALID"));
         assertThrows(RuntimeException.class, () -> nonExistingFile.getContent(new LocalFileReaderImpl()));
+    }
+
+    @Description("If the file exists, then It should return the content")
+    @Test
+    @Tag("unit")
+    public void testContentOfExistingFileShouldReturnContent() {
+        final var content = file.getContent(reader);
+        assertNotNull(content);
+        assertEquals(List.of("TEST"), content);
     }
 }
