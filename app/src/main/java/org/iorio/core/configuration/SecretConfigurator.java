f6c3b76b9df69641674df80a0f76c3eb15ec2357
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-16 17:11:45 +0100

    feat: add directory name in input

--- app/src/main/java/org/iorio/core/configuration/SecretConfigurator.java
+++ app/src/main/java/org/iorio/core/configuration/SecretConfigurator.java
@@ -7,13 +7,13 @@ public class SecretConfigurator {
      * Read secrets from a file and set them as system properties
      * @param fileName the name of the file containing the secrets
      */
-    public static void readSecrets(final String fileName) {
-        final Dotenv dotenv = Dotenv.configure().filename(fileName).load();
+    public static void readSecrets(final String fileName, final String directory) {
+        final Dotenv dotenv = Dotenv.configure().directory(directory).filename(fileName).load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
     }
 
     public static void readTestSecrets() {
-        readSecrets(".env");
+        readSecrets(".env", "");
     }
 
 }
