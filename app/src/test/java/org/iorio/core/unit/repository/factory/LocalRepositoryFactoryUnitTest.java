dc588f35bb9c08572a8b4b75f1e0671740cb9f07
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-17 11:18:36 +0100

    test(unit): add new test for checking output value using mockito

--- app/src/test/java/org/iorio/core/unit/repository/factory/LocalRepositoryFactoryUnitTest.java
+++ app/src/test/java/org/iorio/core/unit/repository/factory/LocalRepositoryFactoryUnitTest.java
@@ -2,8 +2,12 @@ package org.iorio.core.unit.repository.factory;
 
 import jdk.jfr.Description;
 import org.iorio.core.repository.RepositoryFactory;
+import org.iorio.core.repository.local.LocalRepositoryImpl;
+import org.iorio.core.repository.remote.graphql.RemoteRepositoryQLImpl;
+import org.junit.Assert;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.nio.file.Path;
 
@@ -42,5 +46,20 @@ public class LocalRepositoryFactoryUnitTest {
         assertEquals("The repository path does not exist",
                 assertThrows(IllegalArgumentException.class,
                         () -> RepositoryFactory.localRepository("repoName", Path.of("INVALID PATH"))).getMessage());
+    }
+
+    @Description("If all the input values are corect, then the repository should be created")
+    @Test
+    @Tag("unit")
+    public void testLocalRepositoryWithCorrectValues() {
+        final String expectedName = "FinderTest";
+        final Path expectedPath = Path.of("local/path");
+        try(final var mockFactory = Mockito.mockStatic(RepositoryFactory.class)) {
+            mockFactory.when(() -> RepositoryFactory.localRepository("FinderTest", Path.of("local/path")))
+                    .thenReturn(new LocalRepositoryImpl("FinderTest", Path.of("local/path")));
+            final var repository = RepositoryFactory.localRepository("FinderTest", Path.of("local/path"));
+            assertEquals(expectedName, repository.getName());
+            assertEquals(expectedPath, repository.getPath());
+        }
     }
 }
