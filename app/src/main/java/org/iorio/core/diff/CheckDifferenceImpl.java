c258ffd0abc514e9fc95acef6238f41b2bb1bbc3
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-17 11:06:20 +0100

    feat: use GraphQL implementation instead of HTML Scraper

--- app/src/main/java/org/iorio/core/diff/CheckDifferenceImpl.java
+++ app/src/main/java/org/iorio/core/diff/CheckDifferenceImpl.java
@@ -5,33 +5,30 @@ import org.iorio.core.repository.AbstractRepositoryDirectory;
 import org.iorio.core.repository.AbstractRepositoryFile;
 import org.iorio.core.repository.RepositoryFactory;
 import org.iorio.core.repository.local.LocalFileReaderImpl;
+import org.iorio.core.repository.remote.graphql.RemoteFileReaderQLImpl;
 import org.iorio.core.repository.remote.html.RemoteFileReaderImpl;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class CheckDifferenceImpl extends AbstractCheckDifference<Path, URL> {
-    private final static String GITHUB_URL = "https://github.com";
+public class CheckDifferenceImpl extends AbstractCheckDifference<Path, String> {
 
     @Override
-    public List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<URL>>> checkDifference(String user, String repo, String accessToken, String localPath, String branchA, String branchB) {
+    public List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<String>>> checkDifference(String user, String repo, String accessToken, String localPath, String branchA, String branchB) {
         logger.info("Checking difference between branches");
         System.setProperty("GITHUB_TOKEN", accessToken);
-        try {
-            final URL url = URI.create(GITHUB_URL + "/" + user + "/" + repo + "/tree/" + branchA).toURL();
-            BranchSwitcher.switchBranch(localPath, branchB);
-            final var remoteRepository = RepositoryFactory.remoteRepository(repo, url, accessToken);
-            final var localRepository = RepositoryFactory.localRepository(repo, Path.of(localPath));
-            final var localReader = new LocalFileReaderImpl();
-            final var remoteReader = new RemoteFileReaderImpl();
-            return lookForDifferences(localRepository, remoteRepository, localReader, remoteReader);
-        }catch (MalformedURLException e) {
-            throw new IllegalArgumentException("Invalid URL");
-        }
+        BranchSwitcher.switchBranch(localPath, branchB);
+        final var remoteRepository = RepositoryFactory.remoteRepositoryQL(repo, user, branchA, accessToken);
+        final var localRepository = RepositoryFactory.localRepository(repo, Path.of(localPath));
+        final var localReader = new LocalFileReaderImpl();
+        final var remoteReader = new RemoteFileReaderQLImpl(user, repo, branchA);
+        var x = lookForDifferences(localRepository, remoteRepository, localReader, remoteReader);
+        localRepository.getDirectories().forEach(dir -> System.err.println(dir.getFiles()));
+        remoteRepository.getDirectories().forEach(dir -> System.err.println(dir.getFiles()));
+        return x;
     }
 }
