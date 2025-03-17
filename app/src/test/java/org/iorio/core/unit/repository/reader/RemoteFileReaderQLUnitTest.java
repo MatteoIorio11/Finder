c3293cc04bbbd31da5929d3751961bbadb3db581
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-17 09:31:22 +0100

    test(unit): test class RemoteFileReadQL

--- app/src/test/java/org/iorio/core/unit/repository/reader/RemoteFileReaderQLUnitTest.java
+++ app/src/test/java/org/iorio/core/unit/repository/reader/RemoteFileReaderQLUnitTest.java
@@ -1,4 +1,27 @@
 package org.iorio.core.unit.repository.reader;
 
+import jdk.jfr.Description;
+import org.iorio.core.repository.FileReader;
+import org.iorio.core.repository.remote.graphql.RemoteFileReaderQLImpl;
+import org.junit.Test;
+import org.junit.jupiter.api.Tag;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 public class RemoteFileReaderQLUnitTest {
+    private final FileReader<String> reader = new RemoteFileReaderQLImpl("MatteoIorio11",
+            "FinderTest",
+            "main");
+
+    @Description("If the path is valid, then It should be possible to read the Its content")
+    @Test
+    @Tag("unit")
+    public void testContentOfExistingFileShouldExist() {
+        final List<String> content = reader.getContent("file1");
+        assertNotNull(content);
+        assertFalse(content.isEmpty());
+    }
 }
