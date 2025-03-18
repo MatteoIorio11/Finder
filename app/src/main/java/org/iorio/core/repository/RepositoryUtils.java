f631bcff81135fb42f4c110bc647a974a09b1c93
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-18 12:46:12 +0100

    feat(utils): add static method for extract all the common files between two repository

--- app/src/main/java/org/iorio/core/repository/RepositoryUtils.java
+++ app/src/main/java/org/iorio/core/repository/RepositoryUtils.java
@@ -2,8 +2,20 @@ package org.iorio.core.repository;
 
 import org.iorio.core.utils.Pair;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-public class RepositoryUtils<X extends AbstractRepository<?, ?, ?>, Y extends AbstractRepository<?, ?, ?>> {
+public class RepositoryUtils {
     private RepositoryUtils() {}
+
+    public static <X extends AbstractRepository<?, ?, ?>, Y extends AbstractRepository<?, ?, ?>> List<Pair<AbstractRepositoryFile<?>, AbstractRepositoryFile<?>>> commonFilesFromRepository(final X repo1, final Y repo2) {
+        return repo1.getFiles().stream()
+                .filter(file -> repo2.hasFile(file.getName()))
+                .map(file -> new Pair<AbstractRepositoryFile<?>, Optional<? extends AbstractRepositoryFile<?>>>(file, repo2.getFile(file.getName())))
+                .filter(entry -> entry.y().isPresent())
+                .map(pair -> new Pair<AbstractRepositoryFile<?>, AbstractRepositoryFile<?>>(pair.x(), pair.y().get()))
+                .toList();
+    }
 }
