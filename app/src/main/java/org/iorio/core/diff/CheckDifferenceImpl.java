dceb48ad056c4035052b7dc42db9f1731a88ae0e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-18 14:16:02 +0100

    feat: use new logic for checking difference between common files

--- app/src/main/java/org/iorio/core/diff/CheckDifferenceImpl.java
+++ app/src/main/java/org/iorio/core/diff/CheckDifferenceImpl.java
@@ -4,14 +4,15 @@ import org.iorio.core.repository.AbstractRepositoryFile;
 import org.iorio.core.repository.RepositoryFactory;
 import org.iorio.core.repository.local.LocalFileReaderImpl;
 import org.iorio.core.repository.remote.graphql.RemoteFileReaderQLImpl;
+import org.iorio.core.utils.Pair;
+
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
 public class CheckDifferenceImpl extends AbstractCheckDifference<Path, String> {
 
     @Override
-    public List<Map.Entry<AbstractRepositoryFile<Path>, AbstractRepositoryFile<String>>> checkDifference(String user, String repo, String accessToken, String localPath, String branchA, String branchB) {
+    public List<Pair<AbstractRepositoryFile<Path>, AbstractRepositoryFile<String>>> checkDifference(String user, String repo, String accessToken, String localPath, String branchA, String branchB) {
         logger.info("Checking difference between branches");
         System.setProperty("GITHUB_TOKEN", accessToken);
         BranchSwitcher.switchBranch(localPath, branchB);
