5ba6eac8f49189f030fa2844128275785d1f184e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-18 18:29:52 +0100

    feat: use flatmap instead of if (monad style)

--- app/src/main/java/org/iorio/core/repository/RepositoryUtils.java
+++ app/src/main/java/org/iorio/core/repository/RepositoryUtils.java
@@ -15,12 +15,9 @@ public class RepositoryUtils {
             final X repo1,
             final Y repo2) {
         return filterAndMap(repo1.getFiles(),
-                (file) -> {
-                    if (repo2.getFile(file.getName()).isPresent()) {
-                        return Optional.of(new Pair<>(file, repo2.getFile(file.getName()).get()));
-                    }
-                    return Optional.empty();
-                });
+                (file) ->
+                    repo2.getFile(file.getName())
+                            .flatMap(f -> Optional.of(new Pair<>(file, f))));
     }
 
     public static <A, B, X extends AbstractRepository<A, AbstractRepositoryDirectory<A, AbstractRepositoryFile<A>>, AbstractRepositoryFile<A>>,
@@ -28,37 +25,27 @@ public class RepositoryUtils {
             final X repo1,
             final Y repo2) {
         return filterAndMap(repo1.getDirectories(),
-                (directory) -> {
-                    if (repo2.getDirectory(directory.getName()).isPresent()) {
-                        return Optional.of(new Pair<>(directory, repo2.getDirectory(directory.getName()).get()));
-                    }
-                    return Optional.empty();
-                });
+                (directory) ->
+                    repo2.getDirectory(directory.getName())
+                            .flatMap(dir -> Optional.of(new Pair<>(directory, dir))));
     }
 
     public static <A, B, X extends AbstractRepositoryDirectory<A, AbstractRepositoryFile<A>>, Y extends AbstractRepositoryDirectory<B, AbstractRepositoryFile<B>>> List<Pair<AbstractRepositoryDirectory<A, AbstractRepositoryFile<A>>, AbstractRepositoryDirectory<B, AbstractRepositoryFile<B>>>> checkCommonDirectories(
             final X dir1,
             final Y dir2) {
         return filterAndMap(dir1.getInnerDirectories(),
-                (file) -> {
-                    if (dir2.getDirectory(file.getName()).isPresent()) {
-                        return Optional.of(new Pair<>(file, dir2.getDirectory(file.getName()).get()));
-                    }
-                    return Optional.empty();
-                });
+                (file) ->
+                    dir2.getDirectory(file.getName())
+                            .flatMap(dir -> Optional.of(new Pair<>(file, dir))));
     }
 
     public static <A, B, X extends AbstractRepositoryDirectory<A, AbstractRepositoryFile<A>>, Y extends AbstractRepositoryDirectory<B, AbstractRepositoryFile<B>>> List<Pair<AbstractRepositoryFile<A>, AbstractRepositoryFile<B>>> commonFilesFromDirectory(
             final X dir1,
             final Y dir2) {
-        return
-                filterAndMap(dir1.getFiles(),
-                        (file) -> {
-                    if (dir2.getFile(file.getName()).isPresent()) {
-                        return Optional.of(new Pair<>(file, dir2.getFile(file.getName()).get()));
-                    }
-                    return Optional.empty();
-                });
+        return filterAndMap(dir1.getFiles(),
+                        (file) ->
+                                dir2.getFile(file.getName())
+                                        .flatMap(f -> Optional.of(new Pair<>(file, f))));
     }
 
     public static <A, B, X extends AbstractRepositoryFile<A>, Y extends AbstractRepositoryFile<B>> List<Pair<X, Y>> checkForDifferences(
