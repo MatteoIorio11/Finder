e5a6f85494ce5dc2962aca3746b9959c9d98d79d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-19 09:09:02 +0100

    test: init repository utils tests

--- app/src/test/java/org/iorio/core/unit/repository/utils/RepositoryUtilUnitTest.java
+++ app/src/test/java/org/iorio/core/unit/repository/utils/RepositoryUtilUnitTest.java
@@ -1,4 +1,16 @@
 package org.iorio.core.unit.repository.utils;
 
+import jdk.jfr.Description;
+import org.iorio.core.repository.RepositoryUtils;
+import org.iorio.core.repository.local.LocalRepositoryImpl;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class RepositoryUtilUnitTest {
+    @Description("Giving two repositories with common files, It should be possible to retrieve all the common files")
+    @Test
+    @Tag("unit")
+    public void testCommonFilesFromRepository() {
+    }
 }
